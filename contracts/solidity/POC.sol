440fd5887765c0c6b1d6f816c749e2696fb9ce0b
aescuderoc <aescuderoc@EKT1778949MACLAP-4.local>
2022-07-04 16:52:43 -0500

    Version con errores en el contrato y actualizacion de los Servicios

--- contracts/solidity/POC.sol
+++ contracts/solidity/POC.sol
@@ -7,6 +7,11 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract POC {
 
+  //Errors
+  error SearchAlreadyExistError(string _uuid);
+  error SearchDontExistError(string _uuid);
+  error SearchIsClosedError(string uuid);
+
   //Structs used
   struct Search {
     string curp;
@@ -19,19 +24,24 @@ contract POC {
   struct Response {
     address node;
     bool found;
-    uint32 score;
+    string cid;
   }
 
   //State Variables
   address private contractOwner;
   uint16 private requiredResponses;
   mapping (string => Search) internal Searches;
-  //mapping (string => Response[]) internal Responses;
 
   //Events
   event SearchAdded(string _uuid, string _curp, string _cid);
   event ResultAdded(string _uuid, address _node);
   event SearchClosed(string _uuid, string _curp);
+
+  //Modifiers
+  modifier onlyOwner(){
+    require(contractOwner == msg.sender, "not owner");
+    _;
+  }   
 
   constructor(uint16 _requiredResponses) {
     contractOwner = msg.sender;
@@ -39,29 +49,56 @@ contract POC {
   }
   
   function newSearch(string memory _uuid, string memory _curp, string memory _cid) external {
-    //Searches[_uuid] = Search(_curp, _cid, 0, false);
+    if(bytes(Searches[_uuid].curp).length == 0) {
+      revert SearchAlreadyExistError(_uuid);
+    }
     Searches[_uuid].curp = _curp;
     Searches[_uuid].cid = _cid;
     emit SearchAdded(_uuid, _curp, _cid);
   }
 
-  function newResult(string memory _uuid, address _node, bool _found, uint32 _score) external {
-    require(!isOpen(_uuid), "ERR: Search is closed");
-    Searches[_uuid].Responses.push(Response(_node, _found, _score));
+  function newResult(string memory _uuid, bool _found, string memory _cid) external {
+    if(!isOpen(_uuid)) {
+      revert SearchIsClosedError(_uuid);
+    }
+    if(bytes(Searches[_uuid].curp).length != 0) {
+      revert SearchDontExistError(_uuid);
+    }
+    Searches[_uuid].Responses.push(Response(msg.sender, _found, _cid));
     checkforClose(_uuid);
-    emit ResultAdded(_uuid, _node);
+    emit ResultAdded(_uuid, msg.sender);
   }
 
   function checkforClose(string memory _uuid) internal {
     if(Searches[_uuid].Responses.length >= requiredResponses) {
       Searches[_uuid].closed = true;
-      // Se calcula el promedio aqui? o que se hace?
       emit SearchClosed(_uuid, Searches[_uuid].curp);
     }
   }
 
+  function getSearchCID(string memory _uuid) external view returns (string memory) {
+    if(bytes(Searches[_uuid].curp).length != 0) {
+      revert SearchDontExistError(_uuid);
+    }
+    return Searches[_uuid].cid;
+  }
+
+  function getResults(string memory _uuid) external view returns (Response[] memory){
+    if(bytes(Searches[_uuid].curp).length != 0) {
+      revert SearchDontExistError(_uuid);
+    }
+    return Searches[_uuid].Responses;
+  }
+
   function isOpen(string memory _uuid) public view returns(bool) {
+    if(bytes(Searches[_uuid].curp).length != 0) {
+      revert SearchDontExistError(_uuid);
+    }
     return Searches[_uuid].closed;
+  }
+
+  function changeRequiredResponses(uint16 _requiredResponses) external onlyOwner{
+    requiredResponses = _requiredResponses;
   }
   
 }
--- tools/POC.js
+++ tools/POC.js
@@ -10,10 +10,10 @@ const contract = new web3.eth.Contract(ABICODE,CONTRACT_ADDRESS);
 
 class POC {
 
-    busqueda = async(_address, _privateKey, _uuid, _curp, _cid) => {
-        console.log("<busqueda>");
+    addBusqueda = async(_address, _privateKey, _uuid, _curp, _cid) => {
+        console.log("<addBusqueda>");
         let result = false;
-        let encodedABI = contract.methods.NewSearch(_uuid, _curp, _cid).encodeABI();
+        let encodedABI = contract.methods.newSearch(_uuid, _curp, _cid).encodeABI();
         let signedTx = await web3.eth.accounts.signTransaction(
             {
               data: encodedABI,
@@ -38,7 +38,38 @@ class POC {
                 }
             }
         }
-        console.log("</busqueda>");
+        console.log("</addBusqueda>");
+        return result;
+    };
+
+    addResultado = async(_address, _privateKey, _uuid, _found, _cid) => {
+        console.log("<addResultado>");
+        let result = false;
+        let encodedABI = contract.methods.newResult(_uuid, _found, _cid).encodeABI();
+        let signedTx = await web3.eth.accounts.signTransaction(
+            {
+              data: encodedABI,
+              from: _address,
+              gas: 2000000,
+              to: CONTRACT_ADDRESS,
+            },
+            _privateKey,
+            false,
+        );
+        let response = await web3.eth.sendSignedTransaction(signedTx.rawTransaction).catch((err) => {
+            console.error("ERR",err);
+        });
+        const blockNumber = response.blockNumber;
+        let response2 = await contract.getPastEvents("ResultAdded", { fromBlock: blockNumber, toBlock: blockNumber });
+        for(var i=0; i < response2.length; i++){
+            if(response2[i].transactionHash === response.transactionHash){
+                result = {
+                    "uuid": response2[i].returnValues._uuid,
+                    "node": response2[i].returnValues._node,
+                }
+            }
+        }
+        console.log("</addResultado>");
         return result;
     };
 
