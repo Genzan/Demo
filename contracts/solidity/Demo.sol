a7f901bc21f39f1120880d3738461e07615437a2
aescuderoc <aescuderoc@EKT1778949MACLAP-4.local>
2022-08-16 16:47:00 -0500

    Se agregan validaciones para que unicamente el que pide una atestacion pueda revisar los resultados

--- contracts/solidity/Demo.sol
+++ contracts/solidity/Demo.sol
@@ -9,12 +9,22 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Demo {
 
+  //Errors
+  error NotOwnerOfSearch();
+
   //State Variables
   using Counters for Counters.Counter;
   Counters.Counter private _Ids;
-  mapping (bytes32 => string[]) internal Atestaciones;
-  mapping (uint256 => string) internal Busqueda;
+  mapping (bytes32 => atestacion) internal Atestaciones;
+  mapping (uint256 => atestacion) internal Busqueda;
   mapping (address => bool) private whitelist;
+
+  //Structs
+
+  struct atestacion {
+    string[] playground;
+    address owner;
+  }
 
   //Events
   event AtestacionAdded(uint256 _id, string _curp, string _playground);
@@ -31,17 +41,25 @@ contract Demo {
   function Atestacion(string memory _curp, string memory _playground) external {
     _Ids.increment();
     uint256 newItemId = _Ids.current();
-    Atestaciones[keccak256(abi.encodePacked(_curp))].push(_playground);
-    Busqueda[newItemId] = _playground;
+    Atestaciones[keccak256(abi.encodePacked(_curp))].playground.push(_playground);
+    Atestaciones[keccak256(abi.encodePacked(_curp))].owner = msg.sender;
+    Busqueda[newItemId].playground.push(_playground);
+    Busqueda[newItemId].owner = msg.sender;
     emit AtestacionAdded(newItemId, _curp, _playground);
   }
 
   function SearchByCurp(string memory _curp) external view returns (string[] memory) {
-    return (Atestaciones[keccak256(abi.encodePacked(_curp))]);
+    if(Atestaciones[keccak256(abi.encodePacked(_curp))].owner == msg.sender) {
+      revert NotOwnerOfSearch();
+    }
+    return (Atestaciones[keccak256(abi.encodePacked(_curp))].playground);
   }
 
-  function SearchByID(uint256 _id) external view returns (string memory) {
-    return (Busqueda[_id]);
+  function SearchByID(uint256 _id) external view returns (string[] memory) {
+    if(Busqueda[_id].owner == msg.sender) {
+      revert NotOwnerOfSearch();
+    }
+    return (Busqueda[_id].playground);
   }
 
   function addToWhitelist(address _account) external onlyGuest {
--- test/DemoTest.js
+++ test/DemoTest.js
@@ -6,4 +6,10 @@ contract('Prueba unitaria para el contrato EBL', (accounts) => {
     before('Setup para todos los contratos', async function () {
         instanceDemo = await Demo.deployed();
     });
+    it('Se crea una nueva atestacion', async () => {
+        const tx = await instanceDemo.Atestacion("EUCA870810","PruebaDePlayground", { from: accounts[0] });
+        truffleAssert.eventEmitted(tx, 'AtestacionAdded', (ev) => {
+            return true;
+        }, 'ERR','El Evento no fue lanzado');
+    });
 });
